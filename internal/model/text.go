c5340a7bd01c8141adf57629b420e4f58e6ae023
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-06-03 23:28:58 +0600

    Create check expire lifespan text

--- internal/model/text.go
+++ internal/model/text.go
@@ -1,21 +1,28 @@
 package model
 
+import "time"
+
 type Text struct {
-	ID      uint   `json:"id"`
-	Message string `json:"message"`
-	Link    string `json:"link"`
+	ID        uint      `json:"id"`
+	Message   string    `json:"message"`
+	Link      string    `json:"link"`
+	CreatedAt time.Time `json:"createdAt"`
+	ExpiresAt time.Time `json:"expiresAt"`
 }
 
 func (t Text) ToTextResponse() TextResponse {
 	return TextResponse{
-		ID:      t.ID,
-		Message: t.Message,
-		Link:    t.Link,
+		ID:        t.ID,
+		Message:   t.Message,
+		Link:      t.Link,
+		CreatedAt: t.CreatedAt,
+		ExpiresAt: t.ExpiresAt,
 	}
 }
 
 type TextRequest struct {
 	Message string `json:"message"`
+	Day     int    `json:"day"`
 }
 
 func (t TextRequest) ToText() Text {
@@ -25,7 +32,9 @@ func (t TextRequest) ToText() Text {
 }
 
 type TextResponse struct {
-	ID      uint   `json:"id"`
-	Message string `json:"message"`
-	Link    string `json:"link"`
+	ID        uint      `json:"id"`
+	Message   string    `json:"message"`
+	Link      string    `json:"link"`
+	CreatedAt time.Time `json:"createdAt"`
+	ExpiresAt time.Time `json:"expiresAt"`
 }
--- internal/service/text.go
+++ internal/service/text.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"errors"
 	"github.com/jumagaliev1/jiberSoz/internal/model"
 	"github.com/jumagaliev1/jiberSoz/internal/storage"
 	"math/rand"
+	"time"
 )
 
 var (
@@ -22,17 +24,37 @@ func NewTextService(repo *storage.Repository) *TextService {
 
 func (s *TextService) Create(ctx context.Context, request model.TextRequest) (*model.Text, error) {
 	text := request.ToText()
+	text.CreatedAt = time.Now()
+	text.ExpiresAt = text.CreatedAt.AddDate(0, 0, request.Day)
 	text.Link = generateLink()
 
 	return s.repo.Text.Create(ctx, text)
 }
 
 func (s *TextService) GetByLink(ctx context.Context, link string) (*model.Text, error) {
-	return s.repo.Text.GetByLink(ctx, link)
+	text, err := s.repo.Text.GetByLink(ctx, link)
+	if err != nil {
+		return nil, err
+	}
+
+	if checkExpired(text) {
+		return nil, errors.New("text's expired")
+	}
+
+	return text, nil
 }
 
 func (s *TextService) GetByID(ctx context.Context, ID uint) (*model.Text, error) {
-	return s.repo.Text.GetByID(ctx, ID)
+	text, err := s.repo.Text.GetByID(ctx, ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if checkExpired(text) {
+		return nil, errors.New("text's expired")
+	}
+
+	return text, nil
 }
 
 func generateLink() string {
@@ -42,4 +64,12 @@ func generateLink() string {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return string(b)
+}
+
+func checkExpired(text *model.Text) bool {
+	if text.ExpiresAt.Unix() < time.Now().Unix() {
+		return true
+	}
+
+	return false
 }
